12c35233ea862abd81e84f7247403eb1d2048a51
biratlamichhane <lcbirat@gmail.com>
2019-05-20 14:50:32 -0500

    fix error for deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const http = require('http');
 const respond = require('./lib/respond.js');
-const port = process.env.port || 4000;
+const port = process.env.port || 3000 ;
 const server = http.createServer(respond);
 server.listen(port, () => {
     console.log(`App listening on port ${port}!`);
